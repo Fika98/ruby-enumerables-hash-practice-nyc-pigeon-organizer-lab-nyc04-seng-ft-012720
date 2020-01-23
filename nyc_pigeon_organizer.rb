3f3cddd7a12d382feec813a6865c1c601b0f6613
Firuz Afzalov <afzalov98@gmail.com>
2020-01-23 01:03:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
 new_pigeon_data = {}
 
-pigeon_data.each do |pigeon_attribute, hash_of_attribute_values|
+data.each do |pigeon_attribute, hash_of_attribute_values|
   hash_of_attribute_values.each do |attribute_name, pigeon_name|
     pigeon_name.each do |names_of_array|
       if !new_pigeon_data[names_of_array]
