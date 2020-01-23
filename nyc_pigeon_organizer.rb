3fc251bdb045d49e93d40d1cf6e723e94fec4f30
Firuz Afzalov <afzalov98@gmail.com>
2020-01-23 01:13:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,11 +5,11 @@ data.each do |pigeon_attribute, hash_of_attribute_values|
   hash_of_attribute_values.each do |attribute_name, pigeon_name|
     pigeon_name.each do |names_of_array|
       if !new_pigeon_data[names_of_array]
-      new_pigeon_data[names_of_array] = {pigeon_attribute => [attribute_name]}
+      new_pigeon_data[names_of_array] = {pigeon_attribute => [attribute_name.to_s]}
       elsif !new_pigeon_data[names_of_array][pigeon_attribute]
-      new_pigeon_data[names_of_array][pigeon_attribute] = [attribute_name]
+      new_pigeon_data[names_of_array][pigeon_attribute] = [attribute_name.to_s]
       else
-      new_pigeon_data[names_of_array][pigeon_attribute] << attribute_name
+      new_pigeon_data[names_of_array][pigeon_attribute] << attribute_name.to_s
       end
     end
   end
